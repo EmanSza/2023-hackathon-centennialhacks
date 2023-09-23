f2908d436ac16e394bbb15185aff170b242a8076
Nkemjika Obi <nkemjikaobi@gmail.com>
2023-09-23 11:54:39 -0400

    Auth screens

--- client/src/app/components/atoms/Navigation/Navigation.tsx
+++ client/src/app/components/atoms/Navigation/Navigation.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import Icon from '@/app/components/atoms/Icons';
-import { Cabin as cabin } from 'next/font/google';
+import { Inter as inter } from 'next/font/google';
 
 import CustomButton from '../CustomButton/CustomButton';
 import Link from 'next/link';
 
-const cabinFont = cabin({
+const interFont = inter({
 	weight: ['400', '700'],
-	style: ['normal', 'italic'],
+	style: ['normal'],
 	subsets: ['latin'],
 	display: 'swap',
 });
@@ -17,7 +17,10 @@ const Navigation = () => {
 	return (
 		<div className='flex sticky top-0 items-center justify-between font-circular_pro h-[88px] font-medium w-full bg-white text-left py-5 px-4 smallLaptop:px-16 z-50'>
 			<div className='gap-8 w-full h-full justify-between flex'>
-				<Link className='items-center flex gap-1 font-cabi !text-40 font-bold' href='/'>
+				<Link
+					className={`items-center flex gap-1 ${interFont.className} !text-[36px] font-bold`}
+					href='/'
+				>
 					{/* <Icon className='w-[38px] h-10' name='logo' /> */}
 					Centennial Hacks
 				</Link>
